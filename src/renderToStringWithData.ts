4ef2f1b6433167a3a85039a4a2cddd77794a24a4
Gabriel Nunes <gabriel@multiverso.me>
2020-08-21 18:11:30 -0300

    fork commit

---
+++ src/renderToStringWithData.ts
@@ -0,0 +1,9 @@
+import { ReactElement } from 'react';
+import { getMarkupFromTree } from './getDataFromTree';
+
+export function renderToStringWithData(component: ReactElement<any>): Promise<string> {
+  return getMarkupFromTree({
+    tree: component,
+    renderFunction: require("react-dom/server").renderToString,
+  });
+}
